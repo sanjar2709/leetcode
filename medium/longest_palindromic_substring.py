d2d66a698b1d29a1b50d3966f282f5a0288a4c3f
sanjar <sanjarbek2709@gmail.com>
2025-02-24 16:47:21 +0500

    Longest Palindromic Substring fixed

--- medium/longest_palindromic_substring.py
+++ medium/longest_palindromic_substring.py
@@ -3,7 +3,25 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        max_palindrome = ""
+        for i in range(len(s)):
+            palindrome_text1 = self.ExpandAroundCenter(s, i, i)
+            palindrome_text2 = self.ExpandAroundCenter(s, i, i + 1)
+            if len(max_palindrome) <= len(palindrome_text1):
+                max_palindrome = palindrome_text1
+            if len(max_palindrome) <= len(palindrome_text2):
+                max_palindrome = palindrome_text2
+
+        return max_palindrome
+
+    def ExpandAroundCenter(self, s: str, left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        return s[left+1:right]
+
+
 
 solution = Solution()
 result = solution.longestPalindrome(s='babad')
